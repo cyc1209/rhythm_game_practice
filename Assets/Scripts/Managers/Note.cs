9a25e646254029f8803ca5a58c1d377c3b3c1c19
rpgflash <gooding32333@gmail.com>
2020-09-23 17:07:50 +0900

    노트 추가

---
+++ Assets/Scripts/Managers/Note.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float noteSpeed = 400;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Managers/NoteManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform tfNodeAppear = null;
+    [SerializeField] GameObject goNote = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNodeAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+        }
+    }
+}
